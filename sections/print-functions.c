a479a1e48283497bb5e165518ac239e980cc80a0
Herbert Snorrason <odin@anarchism.is>
2021-03-10 14:06:14 +0000

    Use Common Lisp style for unreadable objects.

--- sections/print-functions.c
+++ sections/print-functions.c
@@ -214,7 +214,7 @@ void pstream (object *form, pfun_t pfun) {
 
 void printobject (object *form, pfun_t pfun) {
   if (form == NULL) pfstring(PSTR("nil"), pfun);
-  else if (listp(form) && issymbol(car(form), CLOSURE)) pfstring(PSTR("<closure>"), pfun);
+  else if (listp(form) && issymbol(car(form), CLOSURE)) pfstring(PSTR("#<closure>"), pfun);
   else if (listp(form)) plist(form, pfun);
   else if (integerp(form)) pint(form->integer, pfun);
 #ifdef FLOAT
@@ -227,7 +227,7 @@ void printobject (object *form, pfun_t pfun) {
   else if (arrayp(form)) printarray(form, pfun);
 #endif
 #ifdef CODE
-  else if (form->type == CODE) pfstring(PSTR("code"), pfun);
+  else if (form->type == CODE) pfstring(PSTR("#<code>"), pfun);
 #endif
   else if (streamp(form)) pstream(form, pfun);
   else error2(0, PSTR("Error in print"));
